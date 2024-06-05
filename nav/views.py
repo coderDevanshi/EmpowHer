239f37b85db954f103ce839f7b42e805515d68d7
coderDevanshi <109736075+coderDevanshi@users.noreply.github.com>
2024-06-05 15:02:04 +0530

    Defined the funtion for register where all the HTTP request willnbe posted to database

--- nav/views.py
+++ nav/views.py
@@ -11,4 +11,13 @@ def register(request):
     email=request.POST['email']
     password=request.POST['password']
     confirmation=request.POST['confirmation']
-    branch
+    branch=request.POST['branch']
+    degree=request.POST['degree']
+    gender=request.POST['gender']
+    marks_10=request.POST['marks_10']
+    marks_12=request.POST['marks_12']
+    skill=request.POST['skill']
+    profile=Profile(name=name,email=email,password=password,confirmation=confirmation,branch=branch,)
+
+
+    
