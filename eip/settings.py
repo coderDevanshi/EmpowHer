32e345e7066c6bae768f10e783a37cdc68a33106
coderDevanshi <109736075+coderDevanshi@users.noreply.github.com>
2024-07-05 22:43:54 +0530

    Created frontend for Blog List and and Create/Edit blog

--- eip/settings.py
+++ eip/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+'''STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
